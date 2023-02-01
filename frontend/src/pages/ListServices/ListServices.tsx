38b06c57a726ae2b606ab7249d989165bd44e781
Gustavo Almeida <gustavo_80b@hotmail.com>
2023-01-31 22:18:16 -0300

    Ajuste paginação list services

--- frontend/src/pages/ListServices/ListServices.tsx
+++ frontend/src/pages/ListServices/ListServices.tsx
@@ -1,14 +1,14 @@
-import { useEffect, useState } from "react"
+import { ChangeEvent, EventHandler, useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { AnyAction } from "@reduxjs/toolkit"
 import { createService, getAllServices } from "@src/slices/serviceSlice"
 import { RootState } from "@src/store/store"
 import { ThunkDispatch } from "redux-thunk"
-import { Button, Form, Pagination } from "react-bootstrap"
-import Loading from "@src/components/Loading"
-import AddService from "../AddService/AddService"
+import { Button, Form, Pagination, Col } from "react-bootstrap"
 import { useResetServiceStates } from "@src/hooks/useResetStates"
 import { IPageParams } from "@src/interfaces/IService"
+import AddService from "../AddService/AddService"
+import Loading from "@src/components/Loading"
 
 const ListServices = (): JSX.Element => {
 
@@ -19,7 +19,9 @@ const ListServices = (): JSX.Element => {
   const [showModal, setShowModal] = useState<boolean>(false)
   const [limit, setLimit] = useState<number>(10)
   const [page, setPage] = useState<number>(1)
+  const [calc, setCalc] = useState<number[]>([])
   const [pagination, setPagination] = useState<number[]>([])
+  const [activePagination, setActivePagination] = useState<number>(1)
 
   const limitPage = {
     limit,
@@ -27,17 +29,18 @@ const ListServices = (): JSX.Element => {
   } as IPageParams
 
   useEffect(() => {
-
     dispatch(getAllServices(limitPage))
+  }, [dispatch, limit, page])
 
-    setPagination([])
-
+  useEffect(() => {
     if(services) {
       for(let i = 1; i <= Math.ceil(services.total / services.per_page); i++) {
         pagination.push(i)
       }
+      setCalc([...pagination])
+      setPagination([])
     }
-  }, [dispatch, limit, page])
+  }, [services])
 
   useEffect(() => {
     handleClose()
@@ -55,27 +58,43 @@ const ListServices = (): JSX.Element => {
     resetStates()
   }
 
-  console.log(services)
+  const handleLimit = (e: ChangeEvent<HTMLSelectElement>): void => {
+    setLimit(Number(e.target.value))
+    setPage(1)
+  }
+
+  const handlePage = (page: number) => {
+    setPage(page)
+    setActivePagination(page)
+  }
 
   return (
     <>
     <AddService show={showModal} hide={handleClose} handleSubmit={handleSubmit} />
     <div>ListServices</div>
-    <Form.Select className="my-4" onChange={(e) => setLimit(Number(e.target.value))}>
-      <option>Mostrar quantos por página</option>
+    <Button variant="primary" onClick={() => setShowModal(true)}>Adicionar Serviço</Button>
+    <Form.Select className="my-4" onChange={handleLimit}>
+      <option value="10">Mostrar quantos por página</option>
+      <option value="50">50 Itens</option>
       <option value="10">10 Itens</option>
       <option value="5">5 Itens</option>
       <option value="3">3 Itens</option>
     </Form.Select>
-      <Pagination>
-        {pagination && pagination.map((page) => (
-          <>
-          {console.log('na pagina ' + page)}
-          <Pagination.Item key={page}>{page}</Pagination.Item>
-          </>
-        ))}
-      </Pagination>
-    <Button variant="primary" onClick={() => setShowModal(true)}>Adicionar Serviço</Button>
+    <Col>
+      {services.data && services.data.map((service: any) => (
+        <>
+        {console.log(service)}
+        <p key={service._id}>{service.name}</p>
+        </>
+      ))}
+    </Col>
+    <Pagination>
+      {calc && calc.map((page) => (
+        <>
+        <Pagination.Item key={page} active={page === activePagination} onClick={() => handlePage(page)}>{page}</Pagination.Item>
+        </>
+      ))}
+    </Pagination>
     </>
   )
 }
